4a04f56d65821f01e7268c9bb0bc369856a9efac
Ηλίας Παπαδόδημας <papadodimas@hotmail.gr>
2023-01-23 14:43:09 +0000

    Added 4 files to /src

---
+++ src/serviceA.py
@@ -0,0 +1,11 @@
+class ServiceA:
+    def __init__(self):
+        pass
+    
+    def start(self):
+        # code to start the service
+        pass
+    
+    def call_b_and_c(self):
+        # code to call service B and service C
+        pass
---
+++ src/serviceC.py
@@ -0,0 +1,7 @@
+class ServiceC:
+    def __init__(self):
+        pass
+    
+    def start(self):
+        # code to start the service
+        pass
