e0dee8aea8d77e316de296e11adb9963484912fc
Ηλίας Παπαδόδημας <papadodimas@hotmail.gr>
2023-01-29 11:50:21 +0000

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from serviceA import ServiceA
-from serviceB import ServiceB
-from serviceC import ServiceC
+from src.serviceA import ServiceA
+from src.serviceB import ServiceB
+from src.serviceC import ServiceC
 
 if __name__ == '__main__':
     serviceA = ServiceA()
